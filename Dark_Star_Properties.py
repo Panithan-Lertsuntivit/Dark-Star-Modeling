a652615c741adc6985a4010258762626415bedd1
Panithan Lertsuntivit <panithanblue@gmail.com>
2024-12-25 23:14:57 -0800

    Keeping track of natural and non-natural units

--- Dark_Star_Properties.py
+++ Dark_Star_Properties.py
@@ -23,11 +23,15 @@ import matplotlib.pyplot as plt
 g_spin = 2.0                    # g_s = 2; from Pauli exclusion principle
 # Planck's constant and the speed of light
 Planck_constant = 4.136 * pow(10, -15)      # Units: eV-s
-speed_light = 3.00 * pow(10, 8)             # Units: m/s
+speed_light = 2.99792458 * pow(10, 8)       # Units: m/s
 
 # Reduced Planck's constant [h bar] times the speed of light
 h_bar_c = 197.327                           # Units: MeV-fm
 
+# Gravitational constant
+gravitational_constant = 6.67430 * pow(10, -11)     # Units: m^3 / (kg-s^2)
+G_with_c_squared = 1.189763 * pow(10, 5)    # Units: (fm^3 - c^2) / (MeV-s^2)
+G = G_with_c_squared / pow(speed_light, 2)          # Units: fm^3 / (MeV-s^2)
 
 ''' - - - - - - - - Functions - - - - - - - - '''
 def Yukawa_force(m_chi, m_mu, alpha, x):
@@ -58,6 +62,8 @@ def calculate_pressure_density(x, m_chi, m_mu, alpha):
 
     pressure_density_calculated = np.array((pressure_kinetic + yukawa_potential)
                                 / pow(h_bar_c, 3))
+    # Natural Units: MeV / fm^3
+    # Non-Natural Units: MeV / (fm^3 - c^8)
 
     return pressure_density_calculated
 
@@ -79,21 +85,40 @@ def calculate_energy_density(x, m_chi, m_mu, alpha):
 
     energy_density_calculated = np.array((energy_kinetic + yukawa_potential)
                               / pow(h_bar_c, 3))
+    # Natural Units: MeV / fm^3
+    # Non-Natural Units: MeV / (fm^3 - c^8)
 
     return energy_density_calculated
 
 
 def dark_star_mass_radius(energy_density_array, pressure_density_array):
+    # Initializing array for storing Dark Star mass and radius values
+    darkstar_mass = []
+    darkstar_radius = []
+
+    # Constant Values
 
 
+    # Will be looping through each pressure density value given. Each pressure
+    # density value is used as the central pressure for that dark star
+    for i, central_pressure in enumerate(pressure_density_array):
+        # Initial Values / Dark Star Properties at its core
+        initial_mass = 0.0
+        initial_energy = energy_density_array[i]
+        initial_pressure = central_pressure
+        radial_step = 1.0 * pow(10, 17)             # Units in fm [femto-meter]
 
-    # Initial Values / Dark Star Properties at center (or core)
-    initial_mass = 0.0
-    initial_energy = central_energy_density
-    initial_pressure = central_pressure_density
+        # Current Values
+        # Energy and Pressure have Non-Natural Units of MeV / (fm^3 - c^8)
+        current_energy = initial_energy         # Natural Units: MeV / (fm^3)
+        current_pressure = initial_pressure     # Natural Units: MeV / (fm^3)
+
+        # Non-Natural Units: Mass [MeV / c^8] and Radius [fm]
+        current_mass = initial_mass             # Natural Units: MeV / c^2
+        current_radius = radial_step            # Natural Units: fm
 
 
-    return initial_mass
+    return darkstar_mass, darkstar_radius
 
 
 ''' - - - - - - - - Explanation - - - - - - - - '''
