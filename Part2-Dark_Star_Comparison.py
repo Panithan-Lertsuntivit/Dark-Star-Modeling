c8a9a1fe542ea4d08a1728619bc662ee36136003
Panithan Lertsuntivit <panithanblue@gmail.com>
2025-01-01 23:53:06 -0800

    Unused library

--- Part2-Dark_Star_Comparison.py
+++ Part2-Dark_Star_Comparison.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from scipy.interpolate import splev, splrep
 
 
 ''' - - - - - - - - Functions - - - - - - - - '''
